f65aeacc773055b73d0445626e7b67ed2e7ae3cc
mousechannel <mochenghh@gmail.com>
2024-12-10 19:52:16 +0800

    train script

--- nero2blender.py
+++ nero2blender.py
@@ -18,89 +18,97 @@ if __name__ == '__main__':
     parser.add_argument('--scene', type=str, default="cat", help="scene name")
 
     opt = parser.parse_args()
+    for scene in  os.listdir(opt.path):
+        opt.scene = scene
+        if scene.endswith("blender"):
+            continue
 
-    root = os.path.join(opt.path, opt.scene)
-    output_path = os.path.join(opt.path, opt.scene+"_blender")
-    if not os.path.exists(output_path):
-        os.makedirs(output_path, exist_ok=True)
+        root = os.path.join(opt.path, opt.scene)
+        output_path = os.path.join(opt.path, opt.scene+"_blender")
+        if not os.path.exists(output_path):
+            os.makedirs(output_path, exist_ok=True)
 
-    print(f'[INFO] Data from {root}')
+        print(f'[INFO] Data from {root}')
 
-    img_num = len(glob.glob(f'{root}/*.pkl'))
-    img_ids = [str(k) for k in range(img_num)]
-    cams = [read_pickle(f'{root}/{k}-camera.pkl') for k in range(img_num)]  # pose(3,4)  K(3,3)
-    img_files = [f'{root}/{k}.png' for k in range(img_num)]
-    depth_files = [f'{root}/{k}-depth.png' for k in range(img_num)]
-    points_file = os.path.join(root, "eval_pts.ply")
-    arr = np.linspace(6, 127, 128, dtype=int)
-    import random
-    random.shuffle(arr)  # 先打乱原列表顺序
-    mid = len(arr) // 3
-    test_ids = arr[:mid]
-    train_ids = arr[mid:]
-    # return list1, list2
-    # test_ids, train_ids = read_pickle(os.path.join(opt.path, 'synthetic_split_128.pkl'))
+        img_num = len(glob.glob(f'{root}/*.pkl'))
+        img_ids = [str(k) for k in range(img_num)]
+        cams = [read_pickle(f'{root}/{k}-camera.pkl') for k in range(img_num)]  # pose(3,4)  K(3,3)
+        img_files = [f'{root}/{k}.png' for k in range(img_num)]
+        if len(img_files) == 0:
+            continue
+        depth_files = [f'{root}/{k}-depth.png' for k in range(img_num)]
+        points_file = os.path.join(root, "eval_pts.ply")
+        arr = np.linspace(0, len(img_files)-1, len(img_files), dtype=int)
+        import random
+        random.shuffle(arr)  # 先打乱原列表顺序
+        mid = len(arr) // 4
+        test_ids = arr[:mid]
+        train_ids = arr[mid:]
+        # return list1, list2
+        # test_ids, train_ids = read_pickle(os.path.join(opt.path, 'synthetic_split_128.pkl'))
 
-    # process 2 splits
-    for split in ['train', 'test']:
-        print(f'[INFO] Process transforms split = {split}')
+        # process 2 splits
+        for split in ['train', 'test']:
+            print(f'[INFO] Process transforms split = {split}')
 
-        ids = test_ids if split == "test" else train_ids
-        split_imgs = [img_files[int(i)] for i in ids]
-        split_cams = [cams[int(i)] for i in ids]
+            ids = test_ids if split == "test" else train_ids
+            split_imgs = [img_files[int(i)] for i in ids]
+            split_cams = [cams[int(i)] for i in ids]
 
-        frames = []
-        for image, cam in zip(split_imgs, split_cams):
-            w2c = np.array(cam[0].tolist()+[[0,0,0,1]])
-            c2w = np.linalg.inv(w2c)
-            c2w[:3, 1:3] *= -1  # opencv -> blender/opengl
-            frames.append({
-                'file_path': os.path.join("rgb", os.path.basename(image)).replace(".png",""),
-                'transform_matrix': c2w.tolist(),
-            })
+            frames = []
+            for image, cam in zip(split_imgs, split_cams):
+                w2c = np.array(cam[0].tolist()+[[0,0,0,1]])
+                c2w = np.linalg.inv(w2c)
+                c2w[:3, 1:3] *= -1  # opencv -> blender/opengl
+                frames.append({
+                    'file_path': os.path.join("rgb", os.path.basename(image)).replace(".png",""),
+                    'transform_matrix': c2w.tolist(),
+                })
 
-        fl_x = float(split_cams[0][1][0,0])
-        fl_y = float(split_cams[0][1][1,1])
+            fl_x = float(split_cams[0][1][0,0])
+            fl_y = float(split_cams[0][1][1,1])
 
-        transforms = {
-            'w': 800,
-            'h': 800,
-            'fl_x': fl_x,
-            'fl_y': fl_y,
-            'cx': 400,
-            'cy': 400,
-            # 'aabb_scale': 2,
-            'frames': frames,
-        }
+            transforms = {
+                'w': 800,
+                'h': 800,
+                'fl_x': fl_x,
+                'fl_y': fl_y,
+                'cx': 400,
+                'cy': 400,
+                # 'aabb_scale': 2,
+                'frames': frames,
+            }
 
-        # write json
-        json_out_path = os.path.join(output_path, f'transforms_{split}.json')
-        print(f'[INFO] write to {json_out_path}')
-        with open(json_out_path, 'w') as f:
-            json.dump(transforms, f, indent=2)
-    
-    # write imgs
-    img_out_path = os.path.join(output_path, "rgb")
-    if not os.path.exists(img_out_path):
-        os.makedirs(img_out_path, exist_ok=True)
-    print(f'[INFO] Process rgbs')
-    print(f'[INFO] write to {img_out_path}')
-    for img_id in img_ids:
-        depth = imread(f'{root}/{img_id}-depth.png')
-        depth = depth.astype(np.float32) / 65535 * 15
-        mask = depth < 14.5
-        mask = (mask[...,None] * 255).astype(np.uint8)
+            # write json
+            json_out_path = os.path.join(output_path, f'transforms_{split}.json')
+            print(f'[INFO] write to {json_out_path}')
+            with open(json_out_path, 'w') as f:
+                json.dump(transforms, f, indent=2)
 
-        image = imread(f'{root}/{img_id}.png')[..., :3]
-        image = np.concatenate([image, mask], axis=-1)
+        # write imgs
+        img_out_path = os.path.join(output_path, "rgb")
+        if not os.path.exists(img_out_path):
+            os.makedirs(img_out_path, exist_ok=True)
+        print(f'[INFO] Process rgbs')
+        print(f'[INFO] write to {img_out_path}')
+        for img_id in img_ids:
+            depth = imread(f'{root}/{img_id}-depth.png')
+            depth = depth.astype(np.float32) / 65535 * 15
+            mask = depth < 14.5
+            mask = (mask[...,None] * 255).astype(np.uint8)
 
-        imsave(f'{img_out_path}/{img_id}.png', image)
+            image = imread(f'{root}/{img_id}.png')[..., :3]
+            image = np.concatenate([image, mask], axis=-1)
 
-    # copy ply
-    points_out_path = os.path.join(output_path, "points.ply")
-    shutil.copy2(points_file, points_out_path)
+            imsave(f'{img_out_path}/{img_id}.png', image)
 
-    print("[INFO] Finished.")
+        # copy ply
+        points_out_path = os.path.join(output_path, "points.ply")
+        shutil.copy2(points_file, points_out_path)
+
+        print("[INFO] Finished "+ scene)
+
+    print("[INFO] Finished all")
 
 
 
