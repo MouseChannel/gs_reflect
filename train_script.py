0954c01708eb7cb42f5aab247bc4a74cd4cb8e3f
mousechannel <mochenghh@gmail.com>
2024-12-10 19:54:47 +0800

    train script

--- train_script.py
+++ train_script.py
@@ -2,7 +2,8 @@ import os
 
 
 # -s data/horse_blender --eval -m output/horse_blender -w --brdf_dim 1 --sh_degree -1 --lambda_predicted_normal 2e-1 --brdf_env 512
-datasets = ["angel_blender","bell_blender","cat_blender","horse_blender","luyu_blender","potion_blender","tbell_blender","teapot_blender"]
+datasets = ["angel_blender","bell_blender","cat_blender","luyu_blender","potion_blender","tbell_blender","teapot_blender"]
+# "horse_blender",
 common_args = " --eval -w --brdf_dim 1 --sh_degree -1 --lambda_predicted_normal 2e-1 --brdf_env 512"
 for dataset in datasets:
     os.system("python super_train.py -s " + "datasets/"+dataset +" -m " + "output/"+dataset +  common_args)
