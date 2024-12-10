3264cd087b6baf95c12ace267dada63f84faa447
mousechannel <mochenghh@gmail.com>
2024-12-10 22:53:26 +0800

    fix

--- train_script.py
+++ train_script.py
@@ -4,7 +4,8 @@ import os
 # -s data/horse_blender --eval -m output/horse_blender -w --brdf_dim 1 --sh_degree -1 --lambda_predicted_normal 2e-1 --brdf_env 512
 # datasets = ["angel_blender","bell_blender","cat_blender","luyu_blender","potion_blender","tbell_blender","teapot_blender"]
 
-datasets_source = "/home/mousechannel/project/gs/dataset/forreflect"
+# datasets_source = "/home/mousechannel/project/gs/dataset/forreflect"
+datasets_source = "datasets"
 datasets =os.listdir(datasets_source)
 # "horse_blender",
 common_args = " --eval -w --brdf_dim 1 --sh_degree -1 --lambda_predicted_normal 2e-1 --brdf_env 512"
